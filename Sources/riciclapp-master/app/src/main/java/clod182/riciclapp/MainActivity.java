8d42e529d8c2d641a95a4f2a393d867f8d13e562
FerrozPro <ferrotti.elia@gmail.com>
2018-01-16 16:14:49 +0100

    v2

---
+++ Sources/riciclapp-master/app/src/main/java/clod182/riciclapp/MainActivity.java
@@ -0,0 +1,13 @@
+package clod182.riciclapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
