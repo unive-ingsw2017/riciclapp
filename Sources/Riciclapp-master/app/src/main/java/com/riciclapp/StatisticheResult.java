d94cef00b48d6bf5d2aac2ad1349fb96bb6930e3
FerrozPro <ferrotti.elia@gmail.com>
2018-01-17 16:28:18 +0100

    stat_activity

---
+++ Sources/Riciclapp-master/app/src/main/java/com/riciclapp/StatisticheResult.java
@@ -0,0 +1,22 @@
+package com.riciclapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class StatisticheResult extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat_result);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
